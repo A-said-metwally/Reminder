31d899afcb73d2f63802dc7b5f15729420959c4d
Ahmed Said <86842879+A-said-metwally@users.noreply.github.com>
2021-08-07 13:40:28 +0200

    Create a React app and publish it to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,103 @@
-import logo from './logo.svg';
+import React , { Component } from 'react';
+import { add_Reminder ,remove_Reminder , clear_Reminder } from './actions/Index';
+import {connect} from 'react-redux';
+import moment from 'moment';
+import DatePicker from "react-datepicker"
+import "react-datepicker/dist/react-datepicker.css"
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+  state={
+    text: "",
+    date:new Date()
+  }
+
+  render_Reminders = () =>{
+    const {reminders} = this.props
+    return (
+      <ul className="list-group">
+       { reminders.map((reminder) => {
+          return (
+            <li key={reminder.text.id} className="list-group-item">
+              <div className="reminder-data">
+                  <h2>{reminder.text}</h2>
+                  <p>{moment(new Date(reminder.date)).fromNow()}</p>
+              </div>
+              <div className="remove btn btn-danger" onClick={() => this.props.remove_Reminder(reminder.id)}>X</div>
+            </li>
+          )
+        })}
+      </ul>
+    )
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <img src="reminder.jpg"/>
+        <div className="reminder-title">
+          <h2>What Should U Do ?</h2>
+        </div>
+
+        <input 
+            className="input" 
+            type="text" 
+            value = {this.state.text}
+            placeholder="Enter What You Think .... ?"
+            onChange={(e) => this.setState({text:e.target.value})}
+        />
+          <DatePicker
+            selected={this.state.date}
+            showTimeSelect
+            dateFormat="Pp"
+            value={this.state.date}
+            className="input" 
+            onChange={(date) => this.setState({date})} //only when value has changed
+        />
+
+       
+        <button 
+            style={{display:'block', width:'100%'}} 
+            className="btn btn-primary "
+            onClick={ () => {
+                  this.props.add_Reminder(this.state.text , this.state.date)
+                  this.setState({text:"" , date:""})
+              }
+            }
+            >
+            Add Reminder..
+        </button>
+
+        {this.render_Reminders()}
+
+        {this.props.reminders.length > 0 && 
+          <button 
+              style={{display:'block', width:'100%'}} 
+              className="btn btn-danger "
+              onClick={ () => this.props.clear_Reminder()}
+              >
+              Clear All
+          </button>
+        }
+
+
+      </div>
+    );
+    }
 }
 
-export default App;
+// function mapDispatchtoProps(dispatch){
+//   return {
+//     add_Reminder:() => dispatch(add_Reminder())
+//   }
+// }
+
+// function mapStateToProps(state){
+//   return {
+//     reminders :state
+//   }
+// }
+
+export default connect( (state) => {return {reminders:state}} , {add_Reminder , remove_Reminder , clear_Reminder})(App);
---
+++ src/Reducers/Index.js
@@ -0,0 +1,26 @@
+import { ADD_REMINDER , REMOVE_REMINDER ,CLEAR_REMINDER } from "../types";
+
+const Reminders = (state=[] , action) => {
+    let reminders = null;
+
+    if(action.type === ADD_REMINDER ){
+        reminders = [...state , {text:action.text , date:action.date , id:Math.random()}]
+        console.log("from reducer" , reminders)
+        return reminders
+    }
+    else if(action.type === REMOVE_REMINDER){
+        reminders = state.filter(reminder => reminder.id !== action.id)
+        return reminders
+    }else if(action.type === CLEAR_REMINDER){
+        reminders = []
+        return reminders
+    }
+    
+    else {
+        return state
+    }
+    
+}
+
+export default Reminders
+
